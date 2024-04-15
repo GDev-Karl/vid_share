98c9be9d97c80543fb3dc6284ddb3483d894c7ab
Karl <tekamkarl@gmail.com>
2024-04-15 18:26:19 +0100

    enrtry point

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { Feed } from '@mui/icons-material';
 
-const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const App = () => (
+  <BrowserRouter>
+    <Box sx={ {backgroundColor: '#000'}}>
+      <Narbar>
+        <Routes>
+          <Route path='/' element={<Feed />} />
+          <Route path='/video/:id' element={<VideoFeed />} />
+          <Route path='/channel/:id' element={<ChannelFeed />} />
+          <Route path='/search/:searchTerm' element={<SearchFeed />} />
+        </Routes>
+      </Narbar>
+    </Box>
+  </BrowserRouter>
+)
 
 export default App
