1db667cec1c988aeb84a9ab62a3d70f4ec4d987c
Karl <tekamkarl@gmail.com>
2024-04-16 12:04:46 +0100

    adding videos option

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,14 +1,26 @@
-//import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import SideBar from "./Sidebar";
+import Videos from "./Videos"
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState(null);
+
+  useEffect(() => {
+    setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+    }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: 
     'column', md: 'row'}}}>
       <Box sx={{ height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 }}}>
-        <SideBar />
+        <SideBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
 
         <Typography className="copyright"
         variant="body2" sx={{ mt: 1.5, 
@@ -19,10 +31,11 @@ const Feed = () => {
 
       <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-          <span style={{ color: "#008CFF" }}>New </span>
+          <span style={{ color: "#008CFF" }}>{selectedCategory} </span>
           <span style={{ color: "#B70000" }}>videos</span>
         </Typography>
         
+        <Videos videos={[]}/>
       </Box>
     </Stack>
   )
